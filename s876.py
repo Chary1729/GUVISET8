a2e97535414a3b20b9402d55b3998146c80a2c20
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-07-06 13:35:38 +0530

    Update s876.py

--- s876.py
+++ s876.py
@@ -1,1 +1,9 @@
 
+myinput=int(input())
+temp=myinput
+if(myinput==2):
+  print('no')
+elif(myinput % 1 ==0 and myinput % temp==0 and myinput%2!=0 and myinput%3!=0 and myinput%4!=0):
+  print('no')
+else:
+  print('yes')
