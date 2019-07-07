e55674a5620a5d1e63b8ada80f3135e07ec359c3
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-07-07 22:08:23 +0530

    Update S879.py

--- S879.py
+++ S879.py
@@ -1,7 +1,7 @@
-num1,num2=map(int,input().split())
-prod=num1*num2
-for i in range(0,prod+1):
- if(i**2==num1*num2):
+nums1,num2=map(int,input().split())
+prod=nums1*num2
+for k in range(0,prod+1):
+ if(k**2==nums1*num2):
   print("yes")
   break
 else:
